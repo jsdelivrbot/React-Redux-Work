f3aafdf68e5b1dde063f90d13581034861da5c63
cjwoodru2 <cjwoodru2@gmail.com>
2017-05-07 14:29:23 -0400

    youtube player app beginning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from "./components/search_bar";
+
+const API_KEY='AIzaSyD5YRi9nANkFkNiKiw6p_8ioIlcSZoy4Vo';
 // create new componenet
-const App = function () {
-  return <div>Hi!</div>
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
+
+// TUrn compotnent into HTML & Put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
