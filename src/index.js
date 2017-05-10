aae93bf3e1c0d2dc0cb0aa15dfa1bc4ab970beaf
cjwoodru2 <cjwoodru2@gmail.com>
2017-05-10 19:44:40 -0400

    passing props to list and list item

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from "./components/search_bar";
-
+import VideoList from "./components/video_list";
 const API_KEY='AIzaSyD5YRi9nANkFkNiKiw6p_8ioIlcSZoy4Vo';
+
 // create new componenet
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key:API_KEY, term: 'Surfboards' }, (videos) => {
+      this.setState({ videos });
+
+      console.log(this.state.videos);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    )
+  }
 }
 
 // TUrn compotnent into HTML & Put it in the DOM
