bdea1c715a0b9ae05e745341ef2c94086267d5d5
cjwoodru2 <cjwoodru2@gmail.com>
2017-05-11 19:04:06 -0400

    add lodash to throttle searching - 500ms delay in making req

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
   constructor (props) {
     super(props);
 
-    this.state = { term: 'placeholder' };
+    this.state = { term: null };
   }
   render() {
     return (
