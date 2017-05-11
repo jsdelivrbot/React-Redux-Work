517546b9e0e03fef7759f429e43cd9f0a86d18b7
cjwoodru2 <cjwoodru2@gmail.com>
2017-05-11 18:57:02 -0400

    working search bar ,styled basic and working click side bar video to play

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
-  const { video } = props;
+  const { video, onVideoSelect } = props;
   const imageUrl = video.snippet.thumbnails.default.url;
   const title = video.snippet.title;
 
   return (
-    <li className="list-group-item">
+    <li onClick={ () => onVideoSelect(video) } className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={ imageUrl }/>
--- src/index.js
+++ src/index.js
@@ -11,19 +11,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({ key:API_KEY, term: 'Surfboards' }, (videos) => {
-      this.setState({ videos });
-
+  videoSearch(term) {
+    YTSearch({ key:API_KEY, term }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={ this.state.videos } />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+           videos={ this.state.videos } />
       </div>
     )
   }
